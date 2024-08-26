3155e8272eb38a46e3a197a6412a06772145c8de
ngumi22 <mwaurapngumi@gmail.com>
2024-08-26 17:06:11 +0300

    Remove Handlebars and instead use react-mail to send verification email

--- lib/emailVerification.tsx
+++ lib/emailVerification.tsx
@@ -1,12 +1,11 @@
 import nodemailer from "nodemailer";
-
-import * as handlebars from "handlebars";
-
-import { VerifyEmail } from "./templates/email";
+import { render } from "@react-email/components";
+import VerifyEmail from "./templates/email";
 
 export async function sendVerificationEmail(email: string, token: string) {
   const { EMAIL_USER, EMAIL_PASS } = process.env;
 
+  // Create nodemailer transport
   const transport = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -16,35 +15,31 @@ export async function sendVerificationEmail(email: string, token: string) {
   });
 
   try {
-    await transport.verify(); // Verify the connection
+    // Verify SMTP connection
+    await transport.verify();
   } catch (error) {
     console.error("SMTP connection error:", error);
     return;
   }
 
   try {
+    // Render the email HTML
+    const emailHtml = await render(
+      <VerifyEmail url={`${process.env.BASE_URL}/api/verify?token=${token}`} />
+    );
+
+    // Define email options
     const mailOptions = {
       from: EMAIL_USER,
       to: email,
       subject: "Please verify your email",
-      html: compileWelcomeTemplate(token),
+      html: emailHtml, // Awaited the promise here
     };
 
+    // Send the email
     await transport.sendMail(mailOptions);
     console.log(`Verification email sent to ${email}`);
   } catch (error) {
     console.error("Error sending verification email:", error);
-  }
-}
-
-export function compileWelcomeTemplate(token: string): string {
-  try {
-    const verificationLink = `${process.env.BASE_URL}/api/verify?token=${token}`;
-    const template = handlebars.compile(VerifyEmail);
-    const htmlBody = template({ url: verificationLink });
-    return htmlBody;
-  } catch (error) {
-    console.error("Error compiling Handlebars template:", error);
-    return "";
   }
 }
--- lib/templates/email.tsx
+++ lib/templates/email.tsx
@@ -1,254 +1,181 @@
-export const VerifyEmail = `<!DOCTYPE html>
-<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
+import * as React from "react";
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
 
-<head>
-	<title></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"><!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]--><!--[if !mso]><!--><!--<![endif]-->
-	<style>
-		* {
-			box-sizing: border-box;
-		}
+interface VerifyEmailProps {
+  url: string;
+}
 
-		body {
-			margin: 0;
-			padding: 0;
-		}
+const baseUrl = process.env.BASE_URL ? `https://${process.env.BASE_URL}` : "";
 
-		a[x-apple-data-detectors] {
-			color: inherit !important;
-			text-decoration: inherit !important;
-		}
+export default function VerifyEmail({ url }: VerifyEmailProps) {
+  return (
+    <Html>
+      <Head />
+      <Preview>Bernzz Email Verification</Preview>
+      <Body style={main}>
+        <Container style={container}>
+          <Section style={coverSection}>
+            <Section style={imageSection}>
+              <Img
+                src={`${baseUrl}/logooo.png`} // Ensure this path is correct
+                width="75"
+                height="45"
+                alt="Bernzz's Logo"
+              />
+            </Section>
+            <Section style={upperSection}>
+              <Heading style={h1}>Verify your email address</Heading>
+              <Text style={mainText}>
+                Thanks for starting the new Bernzz account creation process. We
+                want to make sure it's really you. Please click the following
+                verification link when prompted. If you don&apos;t want to
+                create an account, you can ignore this message.
+              </Text>
+              <Section style={verificationSection}>
+                <Button style={ButtonStyle} href={url}>
+                  Join the team
+                </Button>
 
-		#MessageViewBody a {
-			color: inherit;
-			text-decoration: none;
-		}
+                <Text style={validityText}>
+                  (This link is valid for 10 minutes)
+                </Text>
+              </Section>
+            </Section>
+            <Hr />
+            <Section style={lowerSection}>
+              <Text style={cautionText}>
+                Bernzz will never email you and ask you to disclose or verify
+                your password, credit card, or banking account number.
+              </Text>
+            </Section>
+          </Section>
+          <Text style={footerText}>
+            This message was produced and distributed by Bernzz Digital
+            Solutions,© 2024.
+            <Link
+              href="https://bernzzdigitalsolutions.com"
+              target="_blank"
+              style={link}>
+              privacy policy
+            </Link>
+            .
+          </Text>
+        </Container>
+      </Body>
+    </Html>
+  );
+}
 
-		p {
-			line-height: inherit
-		}
+const main = {
+  backgroundColor: "#fff",
+  color: "#212121",
+};
 
-		.desktop_hide,
-		.desktop_hide table {
-			mso-hide: all;
-			display: none;
-			max-height: 0px;
-			overflow: hidden;
-		}
+const ButtonStyle = {
+  backgroundColor: "#000000",
+  padding: "10px 20px",
+  border: "2px solid #000000",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "20px",
+  fontWeight: "bold",
+  textAlign: "center" as const,
+  borderRadius: "8px",
+  color: "#fff",
+  margin: "auto",
+};
 
-		.image_block img+div {
-			display: none;
-		}
+const container = {
+  padding: "20px",
+  margin: "0 auto",
+  backgroundColor: "#eee",
+};
 
-		sup,
-		sub {
-			line-height: 0;
-			font-size: 75%;
-		}
+const h1 = {
+  color: "#333",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "20px",
+  fontWeight: "bold",
+  marginBottom: "15px",
+};
 
-		@media (max-width:520px) {
-			.desktop_hide table.icons-inner {
-				display: inline-block !important;
-			}
+const link = {
+  color: "#2754C5",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "14px",
+  textDecoration: "underline",
+};
 
-			.icons-inner {
-				text-align: center;
-			}
+const text = {
+  color: "#333",
+  fontFamily:
+    "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  fontSize: "14px",
+  margin: "24px 0",
+};
 
-			.icons-inner td {
-				margin: 0 auto;
-			}
+const imageSection = {
+  backgroundColor: "#feda00",
+  display: "flex",
+  padding: "20px 0",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
-			.image_block div.fullWidth {
-				max-width: 100% !important;
-			}
+const coverSection = { backgroundColor: "#fff" };
 
-			.mobile_hide {
-				display: none;
-			}
+const upperSection = { padding: "25px 35px" };
 
-			.row-content {
-				width: 100% !important;
-			}
+const lowerSection = { padding: "25px 35px" };
 
-			.stack .column {
-				width: 100%;
-				display: block;
-			}
+const footerText = {
+  ...text,
+  fontSize: "12px",
+  padding: "0 20px",
+};
 
-			.mobile_hide {
-				min-height: 0;
-				max-height: 0;
-				max-width: 0;
-				overflow: hidden;
-				font-size: 0px;
-			}
+const verifyText = {
+  ...text,
+  margin: 0,
+  fontWeight: "bold",
+  textAlign: "center" as const,
+};
 
-			.desktop_hide,
-			.desktop_hide table {
-				display: table !important;
-				max-height: none !important;
-			}
-		}
-	</style><!--[if mso ]><style>sup, sub { font-size: 100% !important; } sup { mso-text-raise:10% } sub { mso-text-raise:-10% }</style> <![endif]-->
-</head>
+const codeText = {
+  ...text,
+  fontWeight: "bold",
+  fontSize: "12px",
+  margin: "10px 0",
+  textAlign: "center" as const,
+};
 
-<body class="body" style="background-color: #FFFFFF; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
-	<table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #FFFFFF;">
-		<tbody>
-			<tr>
-				<td>
-					<table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f5f5f5;">
-						<tbody>
-							<tr>
-								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px; margin: 0 auto;" width="500">
-										<tbody>
-											<tr>
-												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
-													<table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
-														<tr>
-															<td class="pad" style="padding-bottom:10px;width:100%;padding-right:0px;padding-left:0px;">
-																<div class="alignment" align="center" style="line-height:10px">
-																	<div style="max-width: 125px;"><a href="logooo.png" target="_blank" style="outline:none" tabindex="-1"><img src="logooo.png" style="display: block; height: auto; border: 0; width: 100%;" width="125" alt="bernzzdigitalsolutions logo" title="bernzzdigitalsolutions logo" height="auto"></a></div>
-																</div>
-															</td>
-														</tr>
-													</table>
-												</td>
-											</tr>
-										</tbody>
-									</table>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f5f5f5;">
-						<tbody>
-							<tr>
-								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff; color: #000000; width: 500px; margin: 0 auto;" width="500">
-										<tbody>
-											<tr>
-												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 20px; padding-top: 15px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
-													<table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
-														<tr>
-															<td class="pad" style="padding-bottom:5px;padding-left:5px;padding-right:5px;width:100%;">
-																<div class="alignment" align="center" style="line-height:10px">
-																	<div class="fullWidth" style="max-width: 350px;"><img src="https://d1oco4z2z1fhwp.cloudfront.net/templates/default/2966/gif-resetpass.gif" style="display: block; height: auto; border: 0; width: 100%;" width="350" alt="reset-password" title="reset-password" height="auto"></div>
-																</div>
-															</td>
-														</tr>
-													</table>
-													<table class="heading_block block-2" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
-														<tr>
-															<td class="pad" style="text-align:center;width:100%;">
-																<h1 style="margin: 0; color: #393d47; direction: ltr; font-family: Tahoma, Verdana, Segoe, sans-serif; font-size: 25px; font-weight: normal; letter-spacing: normal; line-height: 120%; text-align: center; margin-top: 0; margin-bottom: 0; mso-line-height-alt: 30px;"><strong>Confirm your email to login?</strong></h1>
-															</td>
-														</tr>
-													</table>
-													<table class="button_block block-3" width="100%" border="0" cellpadding="15" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
-														<tr>
-															<td class="pad">
-																<div class="alignment" align="center"><!--[if mso]>
-<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="{{url}}" style="height:58px;width:229px;v-text-anchor:middle;" arcsize="35%" strokeweight="0.75pt" strokecolor="#FFC727" fillcolor="#ffc727">
-<w:anchorlock/>
-<v:textbox inset="0px,0px,0px,0px">
-<center dir="false" style="color:#393d47;font-family:Tahoma, Verdana, sans-serif;font-size:18px">
-<![endif]--><a href="{{url}}" target="_blank" style="background-color:#ffc727;border-bottom:1px solid #FFC727;border-left:1px solid #FFC727;border-radius:20px;border-right:1px solid #FFC727;border-top:1px solid #FFC727;color:#393d47;display:inline-block;font-family:Tahoma, Verdana, Segoe, sans-serif;font-size:18px;font-weight:undefined;mso-border-alt:none;padding-bottom:10px;padding-top:10px;text-align:center;text-decoration:none;width:auto;word-break:keep-all;"><span style="word-break: break-word; padding-left: 50px; padding-right: 50px; font-size: 18px; display: inline-block; letter-spacing: normal;"><span style="word-break: break-word;"><span style="word-break: break-word; line-height: 36px;" data-mce-style><strong>Confirm Email</strong></span></span></span></a><!--[if mso]></center></v:textbox></v:roundrect><![endif]--></div>
-															</td>
-														</tr>
-													</table>
-												</td>
-											</tr>
-										</tbody>
-									</table>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<table class="row row-3" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f5f5f5;">
-						<tbody>
-							<tr>
-								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px; margin: 0 auto;" width="500">
-										<tbody>
-											<tr>
-												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
-													<table class="paragraph_block block-1" width="100%" border="0" cellpadding="15" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;">
-														<tr>
-															<td class="pad">
-																<div style="color:#393d47;font-family:Tahoma,Verdana,Segoe,sans-serif;font-size:10px;line-height:120%;text-align:center;mso-line-height-alt:12px;">
-																	<p style="margin: 0; word-break: break-word;"><span style="word-break: break-word;">This link will&nbsp;expire in 24 hours.&nbsp;If you continue to have problems</span><br><span style="word-break: break-word;">please feel free to contact us at <a href="mailto:support@youremail.com" target="_blank" title="support@youremail.com" style="text-decoration: underline; color: #393d47;" rel="noopener">support@bernzzdigitalsolutions@gmail.com</a>.</span></p>
-																</div>
-															</td>
-														</tr>
-													</table>
-												</td>
-											</tr>
-										</tbody>
-									</table>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<table class="row row-4" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #fff;">
-						<tbody>
-							<tr>
-								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 500px; margin: 0 auto;" width="500">
-										<tbody>
-											<tr>
-												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
-													<table class="html_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
-														<tr>
-															<td class="pad">
-																<div style="font-family:Arial, Helvetica Neue, Helvetica, sans-serif;text-align:center;" align="center"><div style="height-top: 20px;">&nbsp;</div></div>
-															</td>
-														</tr>
-													</table>
-												</td>
-											</tr>
-										</tbody>
-									</table>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<table class="row row-5" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff;">
-						<tbody>
-							<tr>
-								<td>
-									<table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ffffff; color: #000000; width: 500px; margin: 0 auto;" width="500">
-										<tbody>
-											<tr>
-												<td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 5px; padding-top: 5px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
-													<table class="icons_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; text-align: center; line-height: 0;">
-														<tr>
-															<td class="pad" style="vertical-align: middle; color: #1e0e4b; font-family: 'Inter', sans-serif; font-size: 15px; padding-bottom: 5px; padding-top: 5px; text-align: center;"><!--[if vml]><table align="center" cellpadding="0" cellspacing="0" role="presentation" style="display:inline-block;padding-left:0px;padding-right:0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;"><![endif]-->
-																<!--[if !vml]><!-->
-																<table class="icons-inner" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; display: inline-block; padding-left: 0px; padding-right: 0px;" cellpadding="0" cellspacing="0" role="presentation"><!--<![endif]-->
-																	<tr>
-																		<td style="vertical-align: middle; text-align: center; padding-top: 5px; padding-bottom: 5px; padding-left: 5px; padding-right: 6px;"><a href="http://designedwithbeefree.com/" target="_blank" style="text-decoration: none;"><img class="icon" alt="Beefree Logo" src="logooo.png" height="auto" width="34" align="center" style="display: block; height: auto; margin: 0 auto; border: 0;"></a></td>
-																		<td style="font-family: 'Inter', sans-serif; font-size: 15px; font-weight: undefined; color: #1e0e4b; vertical-align: middle; letter-spacing: undefined; text-align: center; line-height: normal;"><a href="http://designedwithbeefree.com/" target="_blank" style="color: #1e0e4b; text-decoration: none;">Designed with Beefree</a></td>
-																	</tr>
-																</table>
-															</td>
-														</tr>
-													</table>
-												</td>
-											</tr>
-										</tbody>
-									</table>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</td>
-			</tr>
-		</tbody>
-	</table><!-- End -->
-</body>
+const validityText = {
+  ...text,
+  margin: "0px",
+  textAlign: "center" as const,
+};
 
-</html>`;
+const verificationSection = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const mainText = { ...text, marginBottom: "14px" };
+
+const cautionText = { ...text, margin: "0px" };
