7c04a3a9186067c8b02589001974fea73fbfc0f6
Ngumi22 <mwaurapngumi@gmail.com>
2024-10-13 15:36:28 +0300

    Form testing

--- components/Product/Newer.tsx
+++ components/Product/Newer.tsx
@@ -1,9 +1,10 @@
 "use client";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useFormState } from "react-dom";
 import { X } from "lucide-react";
 import { useRef } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -27,125 +28,300 @@ import {
 import { SubmitAction } from "@/lib/productSubmit";
 import { NewProductSchema } from "@/lib/ProductSchema";
 
+type FormValues = z.infer<typeof NewProductSchema>;
+
 export const ProductsForm = () => {
   const [state, formAction] = useFormState(SubmitAction, {
     message: "",
   });
-  const form = useForm<z.output<typeof NewProductSchema>>({
+
+  const form = useForm<FormValues>({
     resolver: zodResolver(NewProductSchema),
     defaultValues: {
       name: "",
       sku: "",
       description: "",
-
+      price: 0,
+      quantity: 0,
+      discount: 0,
       status: "draft",
+      tags: "", // Add a default tag
+      brandName: "",
+      brandImage: undefined,
+      categoryName: "",
+      categoryDescription: "",
+      categoryImage: undefined,
       ...(state?.fields ?? {}),
     },
   });
 
   const formRef = useRef<HTMLFormElement>(null);
+  const brandImageRef = form.register("brandImage");
+  const categoryImageRef = form.register("categoryImage");
 
   return (
-    <Form {...form}>
-      {state?.message !== "" && !state.issues && (
-        <div className="text-red-500">{state.message}</div>
-      )}
-      {state?.issues && (
-        <div className="text-red-500">
-          <ul>
-            {state.issues.map((issue) => (
-              <li key={issue} className="flex gap-1">
-                <X fill="red" />
-                {issue}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <form
-        ref={formRef}
-        className="space-y-8"
-        action={formAction}
-        onSubmit={(evt) => {
-          evt.preventDefault();
-          form.handleSubmit(() => {
-            formAction(new FormData(formRef.current!));
-          })(evt);
-        }}>
-        <div className="flex gap-2">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem className="w-full">
-                <FormLabel>Product Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="sku"
-            render={({ field }) => (
-              <FormItem className="w-full">
-                <FormLabel>SKU</FormLabel>
-                <FormControl>
-                  <Input placeholder="" {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Input placeholder="" {...field} />
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
+    <Card className="space-y-4">
+      <CardHeader>
+        <CardTitle>Create Product</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          {state?.message !== "" && !state.issues && (
+            <div className="text-red-500">{state.message}</div>
           )}
-        />
-
-        <FormField
-          control={form.control}
-          name="status"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Product Status</FormLabel>
-              <Select
-                {...field}
-                value={field.value} // Controlled value
-                onValueChange={(value) => field.onChange(value)} // Update form state on change
-              >
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select status" />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value="pending">Pending</SelectItem>
-                  <SelectItem value="draft">Draft</SelectItem>
-                  <SelectItem value="approved">Approved</SelectItem>
-                </SelectContent>
-              </Select>
-              <FormMessage />
-            </FormItem>
+          {state?.issues && (
+            <div className="text-red-500">
+              <ul>
+                {state.issues.map((issue) => (
+                  <li key={issue} className="flex gap-1">
+                    <X fill="red" />
+                    {issue}
+                  </li>
+                ))}
+              </ul>
+            </div>
           )}
-        />
+          <form
+            ref={formRef}
+            className="space-y-8"
+            action={formAction}
+            onSubmit={(evt) => {
+              evt.preventDefault();
+              form.handleSubmit(() => {
+                formAction(new FormData(formRef.current!));
+              })(evt);
+            }}
+          >
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Card className="h-fit">
+                <CardContent className="space-y-4">
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem className="w-full">
+                        <FormLabel>Product Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="sku"
+                    render={({ field }) => (
+                      <FormItem className="w-full">
+                        <FormLabel>SKU</FormLabel>
+                        <FormControl>
+                          <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl>
+                          <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </CardContent>
+              </Card>
 
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+              <div className="space-y-4">
+                <Card className="h-fit">
+                  <CardHeader>
+                    <CardTitle>Status</CardTitle>
+                  </CardHeader>
+                  <CardContent className="space-y-4">
+                    <FormField
+                      control={form.control}
+                      name="status"
+                      render={({ field }) => (
+                        <FormItem>
+                          <Select
+                            {...field}
+                            value={field.value}
+                            onValueChange={(value) => field.onChange(value)}
+                          >
+                            <FormControl>
+                              <SelectTrigger>
+                                <SelectValue placeholder="Select status" />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              <SelectItem value="pending">Pending</SelectItem>
+                              <SelectItem value="draft">Draft</SelectItem>
+                              <SelectItem value="approved">Approved</SelectItem>
+                            </SelectContent>
+                          </Select>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </CardContent>
+                </Card>
+                <Card className="h-fit">
+                  <CardHeader>
+                    <CardTitle>Product Tags</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <FormField
+                      control={form.control}
+                      name="tags"
+                      render={({ field }) => (
+                        <FormItem>
+                          <Input
+                            type="text"
+                            placeholder="Enter tag"
+                            {...field}
+                          />
+                        </FormItem>
+                      )}
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+
+              <Card className="h-fit">
+                <CardHeader>
+                  <CardTitle>Product Pricing</CardTitle>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                  <FormField
+                    control={form.control}
+                    name="price"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Price</FormLabel>
+                        <Input type="number" placeholder="0" {...field} />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="quantity"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Quantity</FormLabel>
+                        <Input type="number" placeholder="0" {...field} />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="discount"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Discount</FormLabel>
+                        <Input type="number" placeholder="0" {...field} />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </CardContent>
+              </Card>
+
+              <Card className="h-fit">
+                <CardHeader>
+                  <CardTitle>Brand</CardTitle>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                  <FormField
+                    control={form.control}
+                    name="brandName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Brand Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="brandImage"
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormLabel>brandImage</FormLabel>
+                          <FormControl>
+                            <Input type="file" {...brandImageRef} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </CardContent>
+              </Card>
+
+              <Card className="h-fit">
+                <CardHeader>
+                  <CardTitle>Brand</CardTitle>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                  <FormField
+                    control={form.control}
+                    name="categoryName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Category Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="categoryDescription"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Category Description</FormLabel>
+                        <FormControl>
+                          <Input placeholder="" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="categoryImage"
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormLabel>categorymage</FormLabel>
+                          <FormControl>
+                            <Input type="file" {...categoryImageRef} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </CardContent>
+              </Card>
+            </div>
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
--- lib/ProductSchema.tsx
+++ lib/ProductSchema.tsx
@@ -1,5 +1,13 @@
 import * as z from "zod";
 
+const MAX_FILE_SIZE = 500000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
 export const NewProductSchema = z.object({
   name: z
     .string()
@@ -10,6 +18,93 @@ export const NewProductSchema = z.object({
   description: z
     .string()
     .min(5, { message: "Description must be at least 5 characters." }),
-
+  price: z.coerce.number().min(0, { message: "Mininmum price is 0" }),
+  quantity: z.coerce.number().min(0, { message: "Mininmum quantity is 0" }),
+  discount: z.coerce.number().min(0, { message: "Mininmum discount is 0" }),
   status: z.enum(["draft", "pending", "approved"]),
+  tags: z.string(),
+  brandName: z.string().min(2, {
+    message: "Brand name is required and must be at least 2 characters.",
+  }),
+  brandImage: z
+    .custom<FileList>()
+    .transform((val) => {
+      if (val instanceof File) return val;
+      if (val instanceof FileList) return val[0];
+      return null;
+    })
+    .superRefine((file, ctx) => {
+      if (!(file instanceof File)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          fatal: true,
+          message: "Not a file",
+        });
+
+        return z.NEVER;
+      }
+
+      if (file.size > 5 * 1024 * 1024) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Max file size allowed is 5MB",
+        });
+      }
+
+      if (
+        !["image/jpeg", "image/png", "image/webp", "image/jpg"].includes(
+          file.type
+        )
+      ) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "File must be an image (jpeg, jpg, png, webp)",
+        });
+      }
+    })
+    .pipe(z.custom<File>()),
+  categoryName: z.string().min(2, {
+    message: "category name is required and must be at least 2 characters.",
+  }),
+  categoryDescription: z.string().min(2, {
+    message:
+      "category Description is required and must be at least 2 characters.",
+  }),
+  categoryImage: z
+    .custom<FileList>()
+    .transform((val) => {
+      if (val instanceof File) return val;
+      if (val instanceof FileList) return val[0];
+      return null;
+    })
+    .superRefine((file, ctx) => {
+      if (!(file instanceof File)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          fatal: true,
+          message: "Not a file",
+        });
+
+        return z.NEVER;
+      }
+
+      if (file.size > 5 * 1024 * 1024) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Max file size allowed is 5MB",
+        });
+      }
+
+      if (
+        !["image/jpeg", "image/png", "image/webp", "image/jpg"].includes(
+          file.type
+        )
+      ) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "File must be an image (jpeg, jpg, png, webp)",
+        });
+      }
+    })
+    .pipe(z.custom<File>()),
 });
