b94cc82846ce7257e1ecc87f29bfe69c78d9015d
ngumi22 <mwaurapngumi@gmail.com>
2024-11-12 12:49:51 +0300

    Update middleware.ts to avoid cors errors

--- app/api/verify/route.tsx
+++ app/api/verify/route.tsx
@@ -7,21 +7,31 @@ export async function GET(req: NextRequest) {
   const token = searchParams.get("token");
 
   if (!token) {
+    console.error("Token not provided in request");
     return NextResponse.json({ message: "Invalid token" }, { status: 400 });
   }
 
   try {
+    console.log("Decrypting token...");
     const session = await decrypt(token);
     if (!session || !session.userId) {
+      console.error("Token decryption failed or userId missing", session);
       return NextResponse.json({ message: "Invalid token" }, { status: 400 });
     }
 
+    console.log("Establishing database connection...");
     const connection = await getConnection();
+
+    console.log(
+      "Updating user verification status for userId:",
+      session.userId
+    );
     await connection.query(`UPDATE users SET is_verified = TRUE WHERE id = ?`, [
       session.userId,
     ]);
     connection.release();
 
+    console.log("Redirecting after successful verification");
     return NextResponse.redirect(new URL(`/verify?token=${token}`, req.url));
   } catch (error) {
     console.error("Email verification error:", error);
