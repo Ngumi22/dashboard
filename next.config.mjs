c0de8cc68d0c64dc808664a12f4bf57580f42bb6
ngumi22 <mwaurapngumi@gmail.com>
2025-01-25 12:07:16 +0300

    Reconfigure next.config.mjs file to remove allowed orgins

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        allowedOrigins: ['http://localhost:3000' || 'https://dashboard-theta-gilt.vercel.app/'],
-    },
-    async headers() {
-        return [
-            {
-                source: "/api/products",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            },
-            {
-                source: "/api/categories",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+
 }
 
 export default nextConfig
