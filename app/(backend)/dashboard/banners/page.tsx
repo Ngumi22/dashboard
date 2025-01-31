11f5ce2e0508ee79816fe12e86987375d5428631
ngumi22 <mwaurapngumi@gmail.com>
2025-01-31 17:16:14 +0300

    Banner page

--- app/(backend)/dashboard/banners/page.tsx
+++ app/(backend)/dashboard/banners/page.tsx
@@ -4,7 +4,7 @@ import BannerComponent from "@/components/Client-Side/Hero/banners";
 
 export default function Bannerspage() {
   return (
-    <div className="flex-1 scrollbar overflow-x-scroll lg:overflow-hidden flex md:grid md:grid-cols-2 gap-2 md:gap-4 h-32 md:h-96">
+    <div className="p-4">
       <BannerComponent isAdmin />
     </div>
   );
